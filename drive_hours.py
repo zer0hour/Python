d505a32ec9eae30f4592a49f0c5353c300616146
zer0hour <devon.rosenberg@gmail.com>
2020-10-03 10:36:36 +0000

    Added first itteration of drive_hours.py for work

---
+++ drive_hours.py
@@ -0,0 +1,14 @@
+print('years:')
+years = int(input())
+if years >= 1:
+    years = (365 * years)
+
+print('months:')
+months = int(input())
+if months >= 1:
+    months = (30 * months)
+
+print('days')
+days = int(input())
+
+print((years + months + days) * 24)
